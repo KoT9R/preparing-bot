d2da9aeb3bf65cc838539f6a30e2268d2d1c4d9e
KoT9R <vandal5310@gmail.com>
2024-12-15 10:09:43 +0200

    init
    (cherry picked from commit 4ca7a693fc5d3189f16d82805207ee92f4b19362)

---
+++ src/main/kotlin/com/kot/application/Application.kt
@@ -0,0 +1,13 @@
+package com.kot.application
+
+import io.ktor.server.application.*
+import io.ktor.server.netty.EngineMain
+
+fun main(args: Array<String>) {
+    EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
